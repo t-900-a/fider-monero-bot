aa3a57a84b72f6b07255af81e0d0037c08d01e79
t-900-a <t-900@tutanota.com>
2021-06-20 18:38:44 -0400

    process-fider-recent minimum viable complete

--- process-monero/main.go
+++ process-monero/main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/MarinX/monerorpc"
-	wallet "github.com/MarinX/monerorpc/wallet"
-)
-
 func main() {
-	//r.Header.Add("Accept", "application/json")
-	//r.Header.Add("Accept-Encoding", "gzip, deflate, br")
-	//r.Header.Add("Content-Type", "application/json")
-	//r.Header.Add("Authorization", "Bearer vS9U4vSrjVFfkZZ9Rp3RxPoqe8JXyrvvPOps4y2pLuu9w8HgSbsWBuJENREpfyFA")
-	var addressReq wallet.CreateAddressRequest
-	client := monerorpc.New(monerorpc.TestnetURI, nil)
-
-	addressReq.AccountIndex = 0
-	bountyFundingAddressResp, err := client.Wallet.CreateAddress(&addressReq)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println(bountyFundingAddressResp.Address)
-	fmt.Println(bountyFundingAddressResp.AddressIndex)
 
 }
